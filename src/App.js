05de480162687cfe577a14fcabd7f4003862d947
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-27 12:48:32 +0530

    added auto logout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { Switch, Route,Redirect } from 'react-router-dom';
+import { Switch, Route,Redirect,useHistory } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import AuthContext from './store/auth-context';
 
 function App() {
   const authCtx = useContext(AuthContext);
+const history = useHistory();
+
+  useEffect(()=>{
+    setTimeout(()=>{
+          authCtx.logout();
+          console.log('1');
+      },1*60*1000)
+     
+  },[authCtx.login])
   return (
     <Layout>
       <Switch>
